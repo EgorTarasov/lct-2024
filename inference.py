199e2170f2e92c6a92b2ceea840145c99bcd19da
mrapplexz <mr.applexz@gmail.com>
2024-06-15 19:27:39 +0300

    change predict funtion

--- inference.py
+++ inference.py
@@ -20,7 +20,7 @@ class ModelInference:
         self.model = cb.CatBoostClassifier()
         self.model.load_model('artifact/model.cbm')
 
-    def predict(self, unoms: list[int], dates: list[dt.date]):
+    def predict(self, unoms: list[int], dates: list[dt.date], no_threshold: float):
         base_df = load_inference_base(unoms, dates)
         base_df = base_df.set_index(['unom', 'date'])
         for feat in self.features:
@@ -32,13 +32,20 @@ class ModelInference:
             cat_features=features.dtypes[features.dtypes == 'category'].index.tolist()
         )
         out_data = self.model.predict_proba(pool)
-        return pd.DataFrame(data=out_data, index=base_df[['unom', 'date']], columns=self.model.classes_)
+        out_data = pd.DataFrame(data=out_data, columns=self.model.classes_)
+        out_data[['unom', 'date']] = base_df[['unom', 'date']]
+        out_data = out_data[out_data['Нет'] <= no_threshold]
+        out_data = out_data.sort_values(by='date').groupby('unom').first().drop(columns='Нет')
+        out_data['date'] = out_data['date'].apply(lambda x: x.to_pydatetime().date())
+        out_data = out_data.to_dict(orient='index')
+        out_data = {k: {'date': v['date'], 'events': {ke: round(ve, 4) for ke, ve in v.items() if ke != 'date' and ve >= 0.01}} for k, v in out_data.items()}
+        return out_data
 
 
 if __name__ == '__main__':
     pred = ModelInference()
     pred.load()
     unoms = [302, 16460]
-    dates = [dt.date(2024, 4, 6), dt.date(2024, 4, 7)]
-    outs = pred.predict(unoms, dates)
+    dates = [dt.date(2024, 4, x) for x in range(1, 31)]
+    outs = pred.predict(unoms, dates, 0.8)
     print(outs)
